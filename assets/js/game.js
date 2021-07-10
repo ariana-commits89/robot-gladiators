0940e8eba74ef8fbf65080091a7a7c07987af90d
Ariana Fonoti <ariana.fonoti@gmail.com>
2021-07-10 17:23:32 -0600

    robot name

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+/*Note the lack of quotation marks around playerName
+window.alert(playerName);*/
+//What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will od math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+
+
+
+
+
+
+
+
+//fight();
